c38f33eaf1e61d9b7359ee9f74bbf8d928872e57
Bandari Chetan Kumar <chetan@techatcore.com>
2021-02-24 10:47:46 +0530

    add changes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
+//src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", justifyContent: "center", marginTop: "10vh", fontWeight: "bolder"}}>
+      Welcome to Hello React!!
     </div>
   );
 }
-
 export default App;
